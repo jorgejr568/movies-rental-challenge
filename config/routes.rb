272572152514a37a02d09dbd783c44e75ab642ce
Jorge Junior <jorge.junior.568@live.com>
2023-08-16 15:33:18 -0300

    feat: method post for /rent

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :movies, only: %i[index] do
     get :recommendations, on: :collection
     get :user_rented_movies, on: :collection
-    get :rent, on: :member
+    post :rent, on: :member
   end
 
   post '/auth', to: 'users#sign_in'
